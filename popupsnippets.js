c6b6f74336869cea15e7dfdffda048729a132612
SKREPERIST <skreperist666@gmail.com>
2021-04-14 23:02:07 +0300

    dk

---
+++ popupsnippets.js
@@ -0,0 +1,48 @@
+{
+	"Print to console": {
+		"scope": "html",
+		"prefix": "popupform",
+		"body": [
+			"<div id=\"popup\" class=\"popup\">",
+			"  <div class=\"popup__body\">",
+			"    <div class=\"popup__content\">",
+			"      <div class=\"popup__title\">Login</div>",
+			"      <form>",
+			"        <label class=\"popup__label-name\">",
+			"          <input type=\"text\" placeholder=\"Name\" required>",
+			"        </label>",
+			"        <label class=\"popup__label-pass\">",
+			"          <input type=\"text\" placeholder=\"Password\" required>",
+			"        </label>",
+			"        <button class=\"popup__btn\" type=\"submit\">submit</button>",
+			"        <a class=\"popup__account\" href=\"#\">Creat An Account</a>",
+			"        <a class=\"popup__password\" href=\"#\">Lost password?</a>",
+			"      </form>",
+			"    </div>",
+			"  </div>",
+			"</div>"
+		],
+		"description": "added popup with form"
+	}
+}
+
+
+
+{
+	"Popup Simple": {
+		"scope": "html",
+		"prefix": "popupsimpl",
+		"body": [
+			"<div id=\"popup\" class=\"popup\">",
+			"  <div class=\"popup__body\">",
+			"    <div class=\"popup__content\">",
+			"      <a href=\"\" class=\"popup__close\"></a>",
+			"      <div class=\"popup__title\"></div>",
+			"      <div class=\"popup__text\"></div>",
+			"    </div>",
+			"  </div>",
+			"</div>"
+		],
+		"description": "added simple popup"
+	}
+}
